39770d6ef9580e9997959c74f66577580f81df72
Davi Sousa <davi@terradot.earth>
2025-01-26 15:19:00 -0300

    feat: add pre commit

---
+++ backend/app/config.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+
+class Config(BaseModel):
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", "5433")
+    DB_NAME: str = os.getenv("DB_NAME", "mydatabase")
+    DB_USER: str = os.getenv("DB_USER", "myuser")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "mypassword")
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 10080)
+    )
