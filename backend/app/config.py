fe74ae15c522e515721e78c6ef8f6f6a149bea6f
Davi Sousa <davi@terradot.earth>
2025-01-26 17:17:53 -0300

    feat: add alembic

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,11 +8,13 @@ load_dotenv()
 
 class Config(BaseModel):
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
-    DB_PORT: str = os.getenv("DB_PORT", "5433")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
     DB_NAME: str = os.getenv("DB_NAME", "mydatabase")
     DB_USER: str = os.getenv("DB_USER", "myuser")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "mypassword")
 
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "my-secret")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
         os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 10080)
     )
---
+++ backend/app/models.py
@@ -0,0 +1,64 @@
+from datetime import datetime, UTC
+from typing import List
+from sqlalchemy import DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import (
+    Mapped,
+    declarative_base,
+    mapped_column,
+    relationship,
+)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String)
+    avatar_id: Mapped[int] = mapped_column(Integer)
+    email: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+
+    spoilers: Mapped[List["Spoiler"]] = relationship()
+
+
+class Spoiler(Base):
+    __tablename__ = "spoilers"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    card_id: Mapped[int] = mapped_column(ForeignKey("cards.id"))
+    description: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+
+
+class SpoilerSeen(Base):
+    __tablename__ = "spoilers_seen"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    spoiler_id: Mapped[int] = mapped_column(ForeignKey("spoilers.id"))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+
+
+class CardAnswered(Base):
+    __tablename__ = "cards_answered"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    card_id: Mapped[int] = mapped_column(ForeignKey("cards.id"))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
