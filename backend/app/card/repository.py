73e63f0ceee74dcc8d40f097088f53bc4d2f9ff8
Davi Sousa <davi@terradot.earth>
2025-02-23 17:58:30 -0300

    chore: add comment

--- backend/app/card/repository.py
+++ backend/app/card/repository.py
@@ -5,4 +5,5 @@ from app.base.repository import BaseRepository
 
 class CardRepository(BaseRepository[Card]):
     def get_all_card_titles(self) -> List[str]:
+        # Eventually, we should add RAG to this
         return [card.title for card in self.session.query(Card).all()]
